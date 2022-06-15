86aaa9ba2400c5631e65e0c6d286f24c2e566cdc
shaquito25 <arturogabriel25@gmail.com>
2022-06-15 11:21:51 -0400

    Segundo proyecto backend completado

---
+++ db/migrate/20220615135448_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.string :title
+      t.string :content
+      t.bigint :user_id
+      t.bigint :post_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_14_201524) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_15_135448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
